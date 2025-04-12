cd78f82d50d9a92b6ab3fa02ddca21f8b31828a4
Gustavo <gustavoeliashaddad@gmail.com>
2025-04-12 16:14:47 -0300

    feat: configuração inicial de tema

--- front-end/e-commerce/src/main.js
+++ front-end/e-commerce/src/main.js
@@ -1,14 +1,76 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { definePreset } from "@primeuix/themes";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: "{indigo.50}",
+            100: "{indigo.100}",
+            200: "{indigo.200}",
+            300: "{indigo.300}",
+            400: "{indigo.400}",
+            500: "{indigo.500}",
+            600: "{indigo.600}",
+            700: "{indigo.700}",
+            800: "{indigo.800}",
+            900: "{indigo.900}",
+            950: "{indigo.950}",
+        },
+        colorScheme: {
+            light: {
+                surface: {
+                    0: "#ffffff",
+                    50: "{zinc.50}",
+                    100: "{zinc.100}",
+                    200: "{zinc.200}",
+                    300: "{zinc.300}",
+                    400: "{zinc.400}",
+                    500: "{zinc.500}",
+                    600: "{zinc.600}",
+                    700: "{zinc.700}",
+                    800: "{zinc.800}",
+                    900: "{zinc.900}",
+                    950: "{zinc.950}",
+                },
+            },
+            dark: {
+                surface: {
+                    50: "{slate.50}",
+                    100: "{slate.100}",
+                    200: "{slate.200}",
+                    300: "{slate.300}",
+                    400: "{slate.400}",
+                    500: "{slate.500}",
+                    600: "{slate.600}",
+                    700: "{slate.700}",
+                    800: "{slate.800}",
+                    900: "{slate.900}",
+                    950: "{slate.950}",
+                },
+            },
+        },
+    },
+});
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: MyPreset,
+        options: {
+            darkModeSelector: ".my-app-dark",
+        },
+    },
+});
+
+app.mount("#app");
