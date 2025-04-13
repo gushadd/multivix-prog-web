691f03565b914f1ee985fb5b980b02351337277e
gellyorge <gellyorge23@gmail.com>
2025-04-13 20:54:44 -0300

    implementacao de usuario

---
+++ back-end/Api-e-commerce/app/Http/Requests/StoreUsuarioRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUsuarioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|min:3|max:255',
+            'email' => 'required|email|unique:usuarios,email',
+            'password' => 'required|min:6',
+            'data_nascimento' => ['required', 'date', 'before_or_equal:' . now()->subYears(18)->format('Y-m-d')],
+        ];
+    }
+    public function messages()
+{
+    return [
+        'data_nascimento.required' => 'O campo data de nascimento é obrigatório.',
+        'data_nascimento.date' => 'A data de nascimento precisa ser uma data válida.',
+        'data_nascimento.before_or_equal' => 'Você precisa ter pelo menos 18 anos de idade.',
+
+        'nome.required' => 'O campo nome é obrigatório.',
+        'nome.string' => 'O nome precisa ser uma string.',
+        'nome.min' => 'O nome deve ter no mínimo 3 caracteres.',
+        'nome.max' => 'O nome pode ter no máximo 255 caracteres.',
+
+        'email.required' => 'O campo email é obrigatório.',
+        'email.email' => 'O email fornecido não é válido.',
+        'email.unique' => 'Já existe um usuário registrado com este email.',
+
+        'password.required' => 'O campo senha é obrigatório.',
+        'password.min' => 'A senha deve ter no mínimo 6 caracteres.',
+    ];
+}
+
+}
