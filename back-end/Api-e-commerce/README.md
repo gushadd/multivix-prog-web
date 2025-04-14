76f8881c68e92a909e80ca16ff33fa5a415d58d4
gellyorge <gellyorge23@gmail.com>
2025-04-13 22:49:56 -0300

    alteracao no readme e autorizacao das rotas

--- back-end/Api-e-commerce/README.md
+++ back-end/Api-e-commerce/README.md
@@ -1,10 +1,12 @@
-## iniciando a API
+## Iniciando a API
     npm install
     npm run build
     composer install
     php artisan migrate
     php artisan db:seed
+    php artisan serve
 
+## Usuario Padrao
     usuario padrao
     'email' => admin@example.com
     'password' => senhaadmin
--- back-end/Api-e-commerce/routes/api.php
+++ back-end/Api-e-commerce/routes/api.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
+use App\Models\Usuario;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/usuario/criar', [UsuarioController::class,'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
